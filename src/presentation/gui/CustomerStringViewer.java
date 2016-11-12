e61a607cc37bbf90b9d97f4992383135e9642541
lordpdc <lordpdc@hotmail.com>
2016-11-12 13:50:07 -0600

    primera Version de Tab's

---
+++ src/presentation/gui/CustomerStringViewer.java
@@ -0,0 +1,15 @@
+package presentation.gui;
+
+/**
+ * Created by raoman on 12/11/2016.
+ */
+public class CustomerStringViewer {
+    public static String title="Cliente";
+    public static String newCustomer="Nuevo Cliente";
+    public static String deleteCustomer="Borrar Cliente";
+    public static String editCustomer="Editar datos de Cliente";
+
+    public static String searchCustomer="buscar Clientes";
+    public static String searchValues="nombre, telefono, direccion";
+
+}
