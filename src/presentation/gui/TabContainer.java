122aa0b660165af15c42d3cb08a5b0f487e1056f
Cesar Hernandez <cesar.hernandez.mons@gmail.com>
2016-11-12 14:02:28 -0600

    cambios luis

--- src/presentation/gui/TabContainer.java
+++ src/presentation/gui/TabContainer.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 import java.io.IOException;
 
@@ -13,12 +14,15 @@ import java.io.IOException;
 public class TabContainer {
     private Scene MainContanier;
     private final Tab supplierTab = new Tab();
+    Parent root;
+    TabPane tabPane;
 
     public TabContainer(){
         FXMLLoader loader = new FXMLLoader();
         try {
-            Parent root = loader.load(getClass().getResource("TabContainer.fxml").openStream());
+            root = loader.load(getClass().getResource("TabContainer.fxml").openStream());
             MainContanier = new Scene(root);
+            tabPane = (TabPane) MainContanier.lookup("#mainTabContainer");
             this.addTabs();
         } catch (IOException e) {
             e.printStackTrace();
@@ -30,6 +34,6 @@ public class TabContainer {
     }
 
     private void addTabs(){
-
+        //tabPane.getTabs().add();
     }
 }
---
+++ src/presentation/inventory/SupplierTab.java
@@ -0,0 +1,10 @@
+package presentation.inventory;
+
+/**
+ * Created by cesar on 12/11/16.
+ */
+public class SupplierTab {
+
+
+
+}
