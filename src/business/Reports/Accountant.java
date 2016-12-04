70ccc192e03bae5f0900a445a3c8d3ccd18dfd5b
lordpdc <lordpdc@hotmail.com>
2016-12-03 22:28:03 -0600

    ya no se que commitear.... sigo trabajando en los reportes

--- src/business/Reports/Accountant.java
+++ src/business/Reports/Accountant.java
@@ -2,60 +2,35 @@ package business.Reports;
 
 import business.administrator.PurchaseInvoiceAdministrator;
 import business.administrator.SalesInvoiceAdministrator;
-import business.utilities.DefaultValues;
+import business.entities.PurchaseInvoice;
+import business.entities.SaleInvoice;
 
-import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by raoman on 19/11/2016.
  */
-public class Accountant {/*
-    private List salesBook;
-    private List purchasesBook;
+public class Accountant {
+    private List<PurchaseInvoice> purchaseBook;
+    private List<SaleInvoice> salesBook;
 
     public Accountant( ) {
-        SalesInvoiceAdministrator salesAdministrator=new SalesInvoiceAdministrator();
-        salesBook=salesAdministrator.getAllData();
-        PurchaseInvoiceAdministrator purchasAdministrator=new PurchaseInvoiceAdministrator();
-        purchasesBook =purchasAdministrator.getAllData();
-         }
+        SalesInvoiceAdministrator salesAdministrator = new SalesInvoiceAdministrator( );
+        salesBook = salesAdministrator.getAllData( );
+        PurchaseInvoiceAdministrator purchasAdministrator = new PurchaseInvoiceAdministrator( );
+        purchaseBook = purchasAdministrator.getAllData( );
+    }
 
-    public List getSalesBook( ) {
+    public List< PurchaseInvoice > getPurchaseBook( ) {
+        return purchaseBook;
+    }
+
+    public List< SaleInvoice > getSalesBook( ) {
         return salesBook;
     }
-
-    public List getPurchasesBook(Period period ) {
-        return purchasesBook;
-    }
-
     public Report getSalesReport(Period period){
-        List salesForReport=filterBook( period,salesBook );
-        double totalOfReport=calculeTotals( salesForReport );
-        //ReportGenerator reportGenerator=new ReportGenerator( );
-
-        return new Report();
+        SalesReportGenerator generatorSalesReport=new SalesReportGenerator(period) ;
+        return generatorSalesReport.createSalesReport();
     }
-
-    private List filterBook(Period period,List<Invoice> contableBook){
-        List bookFiltered=new ArrayList(  );
-        for(Invoice invoice: contableBook ){
-            Date dateInvoice=invoice.getDate();
-            if(period.isDateIntoPeriod( dateInvoice )){
-                bookFiltered.add( invoice );
-            }
-        }
-        return bookFiltered;
-    }
-    private double calculeTotals(List<Invoice> listInvoiceForReport){
-        Double totalCalculated= DefaultValues.DOUBLE;
-        for( Invoice invoice:listInvoiceForReport ){
-            totalCalculated+=totalCalculated+invoice.getTotalPrice();
-        }
-        return totalCalculated;
-    }
-*/
-
-
 }
+
