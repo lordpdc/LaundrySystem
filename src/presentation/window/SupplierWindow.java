ae0239fd99e2f9c8523c1d0c3cb79c38cc2e335e
Cesar Hernandez <cesar.hernandez.mons@gmail.com>
2016-12-04 01:21:51 -0600

    Tab Funcionando, Boton Crear

--- src/presentation/window/SupplierWindow.java
+++ src/presentation/window/SupplierWindow.java
@@ -1,10 +1,16 @@
 package presentation.window;
 
+import business.administrator.DataAdministrator;
+import business.administrator.SupplierAdministrator;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import presentation.inventory.Inventory;
+import presentation.inventory.InventoryView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +20,7 @@ import java.util.ResourceBundle;
  * Created by Luis on 03/12/2016.
  */
 public class SupplierWindow implements Initializable {
+    InventoryView view;
 
     @FXML protected Button createButton;
     @FXML protected Button cancelButton;
@@ -27,6 +34,30 @@ public class SupplierWindow implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        createButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String name = nameField.getText();
+                String adddress = addressField.getText();
+                String telephone = telephoneField.getText();
+                String email = emailField.getText();
+
+                if (!name.equals("")&&!adddress.equals("")&&!telephone.equals("")&&!email.equals("")){
+                    if (view!=null){
+
+                        //view.addNew(name,adddress,telephone,email);
+                    }else{
+                        System.out.println("Nah dawg...");
+                    }
+                }else{
+                    System.out.println("Nahhh");
+                }
+            }
+        });
+    }
+
+    public void setAdmin(InventoryView view){
+        this.view = view;
     }
 
     public void setData(){
