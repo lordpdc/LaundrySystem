2e17d95667c9b39a0849ee2da4b555d171a400a3
Pinkus96 <zelda_10a@hotmail.com>
2016-12-03 22:00:24 -0600

    intento de controlador

--- src/presentation/gui/InventoryView.java
+++ src/presentation/gui/InventoryView.java
@@ -1,21 +1,35 @@
 package presentation.gui;
 
 import business.entities.Supplier;
+import com.sun.deploy.util.FXLoader;
 import data.dao.SupplierDAO;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
 import presentation.gui.tabs.TabView;
+
+import javax.swing.*;
 
 /**
  * Created by cesar on 01/12/16.
  */
 public class InventoryView extends TabView<Supplier> {
     private SupplierDAO dao = new SupplierDAO();
+    private JFXPanel panel = new JFXPanel();
 
     public InventoryView(){
         super();
         this.tab.setText("Inventario");
         initComponents();
+        JFrame ventana = new JFrame();
+        ventana.add(panel);
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                panel.setScene(new FXLoader().loadFX(););
+            }
+        }unnable);
     }
 
     private void initComponents(){
