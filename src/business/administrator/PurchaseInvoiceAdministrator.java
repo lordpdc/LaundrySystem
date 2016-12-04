584853c50436b215d4bdca4ab59366172759bd5c
lordpdc <lordpdc@hotmail.com>
2016-12-04 12:36:37 -0600

    Administradores obtienes la lista de detalles de la factura

--- src/business/administrator/PurchaseInvoiceAdministrator.java
+++ src/business/administrator/PurchaseInvoiceAdministrator.java
@@ -11,52 +11,29 @@ import java.util.List;
  */
 
 public class PurchaseInvoiceAdministrator extends DataAdministrator<PurchaseInvoice > {
-    private List listDetailPurchase;
-
-    public List getListDetailPurchase( ) {
-        return listDetailPurchase;
-    }
-
-    public void setListDetailPurchase( List listDetailPurchase ) {
-        this.listDetailPurchase = listDetailPurchase;
-    }
 
     public PurchaseInvoiceAdministrator( ) {
         this.setAccessData(new PurchaseInvoiceDAO());
     }
 
-    @Override
-    public void addNew( PurchaseInvoice invoice ) {
-        accessData.create( invoice );
-    }
-
-    @Override
-    public void update( int id, PurchaseInvoice invoice ) {
-        accessData.update( id,invoice );
-    }
 
 
-    public PurchaseInvoice search( String name ) {
-        return null;
-    }
 
-    public List<PurchaseDetail> getDetailPurchase(PurchaseInvoice invoice){
+    public List<PurchaseDetail> getPurchaseDetails(int idInvoice){
         List<PurchaseDetail> detailsList;
-        PurchaseDetailDAO accesDetails=new PurchaseDetailDAO(  );
-
-        detailsList=accesDetails.readAll();
-
-        return filterDetailsList( invoice.getId(),detailsList );
+        PurchaseDetailsAdministrator accesDetails=new PurchaseDetailsAdministrator(  );
+        detailsList=accesDetails.getAllData();
+        return filterAllDetailsList( idInvoice,detailsList );
 
 
     }
-    private List<PurchaseDetail> filterDetailsList(int idInvoice, List<PurchaseDetail> detailsList){
-        List<PurchaseDetail> detailsListFilter=new ArrayList<>(  );
-        for( PurchaseDetail detail: detailsList ) {
+    private List<PurchaseDetail> filterAllDetailsList( int idInvoice, List<PurchaseDetail> allDetails){
+        List<PurchaseDetail> detailsListFiltered=new ArrayList<>(  );
+        for( PurchaseDetail detail: allDetails ) {
             if(detail.getIdPurchaseInvoice()==idInvoice){
-                detailsListFilter.add( detail );
+                detailsListFiltered.add( detail );
             }
         }
-        return detailsListFilter;
+        return detailsListFiltered;
     }
 }
--- src/business/administrator/SaleInvoiceAdministrator.java
+++ src/business/administrator/SaleInvoiceAdministrator.java
@@ -1,7 +1,11 @@
 package business.administrator;
 
-import business.entities.SaleInvoice;
+import business.entities.*;
+import data.dao.PurchaseDetailDAO;
 import data.dao.SalesInvoiceDAO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by raoman on 29/11/2016.
@@ -10,6 +14,24 @@ public class SaleInvoiceAdministrator extends DataAdministrator<SaleInvoice> {
 
     public SaleInvoiceAdministrator( ) {
         setAccessData( new SalesInvoiceDAO( ) );
+    }
+
+    public List<SaleDetails> getSaleDetails( int idInvoice){
+        List<SaleDetails> detailsList;
+        SaleDetailsAdministrator accesDetails=new SaleDetailsAdministrator(  );
+        detailsList=accesDetails.getAllData();
+        return filterAllDetailsList( idInvoice,detailsList );
+
+
+    }
+    private List<SaleDetails> filterAllDetailsList( int idInvoice, List<SaleDetails> allDetails){
+        List<SaleDetails> detailsListFiltered=new ArrayList<>(  );
+        for( SaleDetails detail: allDetails ) {
+            if(detail.getIdSaleInvoice()==idInvoice){
+                detailsListFiltered.add( detail );
+            }
+        }
+        return detailsListFiltered;
     }
 
 }
