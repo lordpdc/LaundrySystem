41a970b0c744d90e996bdf592b809e247d24bcd0
lordpdc <lordpdc@hotmail.com>
2016-12-04 19:12:26 -0600

    ventana de generacion de reportes "lista" falta que la llamen desde laventana principal

--- src/business/Reports/Period.java
+++ src/business/Reports/Period.java
@@ -4,6 +4,7 @@ import business.utilities.DefaultValues;
 import business.utilities.MsgErrorReport;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 /**
  * Created by raoman on 01/12/2016.
@@ -17,43 +18,50 @@ public class Period {
         finalDate = DefaultValues.DATE;
     }
 
-    public Date getInitialDate() {
+    public Date getInitialDate( ) {
         return initialDate;
     }
 
-    public void setInitialDate(Date initialDate) throws Exception {
-        if (isValidInitialDate(initialDate)) {
+    public void setInitialDate( Date initialDate ) throws Exception {
+        if( isValidInitialDate( initialDate ) ) {
             this.initialDate = initialDate;
-        } else {
-            throw new Exception( MsgErrorReport.msgInitialDateInvalid);
+        }else {
+            throw new Exception( MsgErrorReport.msgInitialDateInvalid );
         }
     }
+    public void setInitialDate( LocalDate initialDate) throws Exception {
+        setInitialDate( Date.valueOf(initialDate) );
+    }
+    public void setFinalDate( LocalDate finalDate) throws Exception {
+        setFinalDate( Date.valueOf(finalDate) );
+    }
 
-    public Date getFinalDate() {
+    public Date getFinalDate( ) {
         return finalDate;
     }
 
-    public void setFinalDate(Date finalDate) throws Exception {
-        if (isValidFinalDate(finalDate)) {
+    public void setFinalDate( Date finalDate ) throws Exception {
+        if( isValidFinalDate( finalDate ) ) {
             this.finalDate = finalDate;
-        } else {
-            throw new Exception(MsgErrorReport.msgFinalDateInvalid);
+        }else {
+            throw new Exception( MsgErrorReport.msgFinalDateInvalid );
         }
     }
-    private boolean isValidInitialDate(Date date) {
-        return date != null && date.before(finalDate);
+
+    private boolean isValidInitialDate( Date date ) {
+        return date != null && date.before( finalDate );
     }
 
-    private boolean isValidFinalDate(Date date) {
-        return date != null && date.after(initialDate);
+    private boolean isValidFinalDate( Date date ) {
+        return date != null && date.after( initialDate );
     }
 
-    public boolean isDateIntoPeriod(Date date){
-        return date.before( finalDate )&& date.after( initialDate );
+    public boolean isDateIntoPeriod( Date date ) {
+        return date.before( finalDate ) && date.after( initialDate );
     }
 
     @Override
     public String toString( ) {
-        return "Periodo-"+ initialDate +"-" + finalDate;
+        return "Periodo-" + initialDate + "-" + finalDate;
     }
 }
